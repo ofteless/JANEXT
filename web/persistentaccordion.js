ddb22a951d114bac89898d304ad1bf8f9b592452
ofteless <notnsfwacct7134@gmail.com>
2025-07-28 17:54:34 -0400

    style: delinting

--- web/persistentaccordion.js
+++ web/persistentaccordion.js
@@ -1,26 +1,26 @@
 function saveState(accordionId, isOpen) {
-  localStorage.setItem(`accordion-${accordionId}`, isOpen.toString());
+    localStorage.setItem(`accordion-${accordionId}`, isOpen.toString());
 }
 
 function loadAccordionState(accordionId) {
-  const state = localStorage.getItem(`accordion-${accordionId}`);
-  return state === 'true';
+    const state = localStorage.getItem(`accordion-${accordionId}`);
+    return state === 'true';
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  const accordions = document.querySelectorAll('details');
-  
-  accordions.forEach(accordion => {
-    const accordionId = accordion.id;
-    
-    // load & set
-    const shouldOpen = loadAccordionState(accordionId);
-    if (shouldOpen) {
-      accordion.open = true;
-    }
-    
-    accordion.addEventListener('toggle', function() {
-      saveState(accordionId, this.open);
+    const accordions = document.querySelectorAll('details');
+
+    accordions.forEach(accordion => {
+        const accordionId = accordion.id;
+
+        // load & set
+        const shouldOpen = loadAccordionState(accordionId);
+        if (shouldOpen) {
+            accordion.open = true;
+        }
+
+        accordion.addEventListener('toggle', function() {
+            saveState(accordionId, this.open);
+        });
     });
-  });
 });
