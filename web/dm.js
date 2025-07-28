664edc9e8f77df0bf079d6a9b57defd3f9541afa
ofteless <notnsfwacct7134@gmail.com>
2025-07-28 17:51:28 -0400

    feat: dark mode

--- web/dm.js
+++ web/dm.js
@@ -1,22 +1,34 @@
-function dm() {
-    if (styleDM) {
-        document.body.removeChild(styleDM);
-        dmOn = false;
-    } else {
-        document.body.appendChild(styleDM);
-        dmOn = true;
-    }
-}
-
-
 const styleDM = document.createElement('style');
 styleDM.textContent = `
 body {
-color: white;
-background-color: black;
+    color: white;
+    background-color: black;
 }
 
 hr {
-color: white
+    color: white;
 }
 `;
+
+let dmOn = localStorage.getItem('darkMode') === 'true';
+
+if (dmOn) {
+    document.body.appendChild(styleDM);
+}
+
+function dm() {
+    if (dmOn) {
+        document.body.removeChild(styleDM);
+        dmOn = false;
+        localStorage.setItem('darkMode', 'false');
+    } else {
+        document.body.appendChild(styleDM);
+        dmOn = true;
+        localStorage.setItem('darkMode', 'true');
+    }
+}
+
+const dmButton = document.getElementById('dm');
+if (dmButton) {
+    dmButton.addEventListener("click", dm);
+}
